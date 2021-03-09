95403e2f81a8d7707a0f75246394f3dab81a474d
PlaneCargo191 <59277871+PlaneCargo191@users.noreply.github.com>
2021-03-09 16:17:18 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,108 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { body, query, validationResult } from 'express-validator'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+const SECRET = "SIMPLE_SECRET"
+
+interface JWTPayload {
+  username: string;
+  password: string;
+}
+
+app.post('/login',
+  (req, res) => {
+
+    const { username, password } = req.body
+    // Use username and password to create token.
+    var token = jwt.sign(username, password)
+    if() {
+      return res.status(200).json({
+        message: 'Login succesfully',
+        token: token
+      })
+    } else {
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
+  })
+
+app.post('/register',
+  (req, res) => {
+
+    const { username, password, firstname, lastname, balance } = req.body
+
+    if(username != ) {
+      
+      return res.status(200).json({
+        message: 'Register successful'
+      })
+    } else {
+      return res.status(400).json({
+        message: 'Username is already in used'
+      })
+    }
+  })
+
+app.get('/balance',
+  (req, res) => {
+    const token = req.query.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      return res.status(200).json({
+        name: username
+      })
+    }
+    catch (e) {
+      //response in case of invalid token
+      return res.status(401).json({
+        message: 'Invalid token'
+      })
+    }
+  })
+
+app.post('/deposit',
+  body('amount').isInt({ min: 1 }),
+  (req, res) => {
+    const token = req.query.token as string
+    //Is amount <= 0 ?
+    if (!validationResult(req).isEmpty()) {
+      return res.status(400).json({ 
+        message: "Invalid data" 
+      })
+    } else if(token  {
+
+    }
+  })
+
+app.post('/withdraw',
+  (req, res) => {
+  })
+
+app.delete('/reset', (req, res) => {
+  //code your database reset here
+  const deleteInfo = 
+  return res.status(200).json({
+    message: 'Reset database successfully'
+  })
+})
+
+app.get('/me', (req, res) => {
+  
+})
+
+app.get('/demo', (req, res) => {
+  return res.status(200).json({
+    message: 'This message is returned from demo route.'
+  })
+})
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
